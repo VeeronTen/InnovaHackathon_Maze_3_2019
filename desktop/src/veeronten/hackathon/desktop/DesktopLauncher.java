70cccf370e7e7d3d7ce5b649c5757de64c3e1060
veeronten <veeronten@gmail.com>
2019-03-16 19:12:36 +0300

    init

---
+++ desktop/src/veeronten/hackathon/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package veeronten.hackathon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import veeronten.hackathon.TheMazeGame;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new TheMazeGame(), config);
+    }
+}
